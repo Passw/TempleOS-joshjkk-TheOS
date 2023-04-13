23dcafafa43b2d808e29b658509f5bd9a1e8e4a9
Joshua Kinder <joshjkk0@gmail.com>
2023-04-13 19:47:25 +0100

    Change color system

--- src/kernel/lib/include/graphics.h
+++ src/kernel/lib/include/graphics.h
@@ -3,21 +3,35 @@
 
 #include "stdint.h"
 
+#define LAST_ROW(box) (box->beg_row + box->rows - 1)
+#define LAST_COL(box) (box->beg_col + box->cols - 1)
+
+typedef struct Color {
+	uint8_t fore;
+	uint8_t back;
+} color_t;
+
+typedef struct Colorscheme {
+	color_t title;
+	color_t title_border;
+	color_t border;
+	color_t content;
+} colorscheme_t;
+
 typedef struct Box {
 	char *title;
 	uint8_t beg_row;
 	uint8_t beg_col;
 	uint8_t rows;
 	uint8_t cols;
-	uint8_t colorscheme[10];
-	int title_menu;
+	colorscheme_t colorscheme;
 } box_t;
 
-box_t *init_box(char *title, uint8_t beg_row, uint8_t beg_col, uint8_t rows, uint8_t cols, uint8_t colorscheme[10], int title_menu);
+box_t *init_box(char *title, uint8_t beg_row, uint8_t beg_col, uint8_t rows, uint8_t cols);
 
 void outline(uint8_t beg_row, uint8_t beg_col, uint8_t rows, uint8_t cols);
 
-void fill(uint8_t beg_row, uint8_t beg_col, uint8_t rows, uint8_t cols);
+void fill(uint8_t beg_row, uint8_t beg_col, uint8_t rows, uint8_t cols, color_t color);
 
 void refresh_box(box_t *box);
 
